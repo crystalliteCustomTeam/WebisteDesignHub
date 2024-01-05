ab191faaee4cb8e24cb9bde5c1d4946fcadd63d1
dev-sikander <sikander@crystallitedigital.com>
2024-01-05 21:58:44 +0500

    Web-development

---
+++ src/app/website-development/page.jsx
@@ -0,0 +1,131 @@
+// Import Page Components
+import Hero from "@/components/hero/hero";
+import Services from "@/components/website-development/services/services";
+import SmallBanner from "@/components/smallbanner/smallbanner";
+import Packages from "@/components/packages/packages";
+import Portfolios from "@/components/portfolios/portfolios";
+import Reviews from "@/components/reviews/reviews";
+import Weare from "@/components/weare/weare";
+import Contact from "@/components/contact/contact";
+import Letstalk from "@/components/letstalk/letstalk";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import Script from "next/script";
+// Import Images
+import smallBannerImage from "media/website-development/smallBanner.png";
+import portfolioOne from "media/logo-design/portfolios/one.png";
+import portfolioTwo from "media/logo-design/portfolios/two.png";
+import portfolioThree from "media/logo-design/portfolios/three.png";
+import portfolioFour from "media/logo-design/portfolios/four.png";
+import portfolioFive from "media/logo-design/portfolios/five.png";
+import PopupAlert from "@/components/popup-alert/popup-alert";
+
+const Page = () => {
+    // Array for Hero Component Props
+    const hero = {
+        page: "websiteDevelopment",
+        subtitle: "Website Design & Development",
+        title: "100% Custom Websites By Top Website Development Company",
+        desc: "Tired of generic websites that don't make an impact? Experience the difference with our top design and website development company where we uniquely represent your brand and drive results.",
+        points: ["Optimized Performance", "Responsive Design", "Brand Consistency", "Search Engine Visibility"],
+        bg: "bg-[#0f1013]",
+        video: "videos/websiteDevelopment.mp4",
+        isForm: false,
+        WebBanner: true,
+        btnBg: "bg-[#283C3C]",
+        btnBorder: "border-[#283C3C]",
+        btnHover: "hover:bg-[#283C3C]"
+    };
+    // Array for Small Banner Component Props
+    const smallbanner = {
+        page: "websiteDevelopment",
+        title: "Let us develop a responsive website to help you attract more visitors",
+        desc: "Our professional web development team is at your service, dedicated to design and develop dynamic websites that urges visitors to take your desired action on the website.",
+        img: smallBannerImage,
+        bgColor: "bg-[#0f1013]",
+        btnBg: "bg-[#283C3C]",
+        btnBorder: "border-[#283C3C]",
+        btnHover: "hover:bg-[#283C3C]",
+        theme: "dark"
+    };
+    // Array for Portfolios Component Props
+    const portfolios = {
+        title: "Reliability Guaranteed At Our Web Development Company",
+        desc: "Take a look at our web design and development projects. They're like examples of our work, showing how we bring ideas to life on the internet. ",
+        images: [portfolioOne, portfolioTwo, portfolioThree, portfolioFour, portfolioFive],
+        theme: "light"
+    };
+    // Array for Packages Component Props
+    const packages = {
+        subtitle: "Web Development Packages",
+        title: "Budget-Friendly Website Development Company",
+        desc: "Our commitment to affordability means you can have a stunning, professionally designed website that perfectly suits your budget.",
+        key: 1,
+        name: "Website Development"
+    }
+    // Array for Letstalk Component Props
+    const letstalk = {
+        title: "Design Consultation",
+        desc: "Contact us for a free consultation, and let's design your captivating logo, website, and mobile app together!"
+    }
+    // Array for Reviews Component Props
+    const reviews = [
+        {
+            message: "The experience of working with Web Design Hub has been wonderful. They have surpassed our expectations at every turn, starting with the initial consultation and ending with the launch. Their team's dedication to detail, originality, and technological expertise were essential to realizing our vision. Due to their dedication to quality, our website not only looks amazing but also runs without a hitch. Anyone looking for top-notch web development services should get in contact with them immediately.",
+            name: "John Mitchell",
+            jd: false,
+            profile: "/website-development/reviews/1.png"
+        },
+        {
+            message: "For years, Web Design Hub has been our dependable website development partner, and they never cease to wow us. It's amazing how well they can translate complicated concepts into web pages that are easy to use. The team's commitment to comprehending our brand and goals has produced websites that are an ideal representation of us. Their superb post-launch assistance makes them the ideal long-term partner. I am quite pleased with their services.",
+            name: "Sarah Anderson",
+            jd: false,
+            profile: "/website-development/reviews/2.png"
+        },
+        {
+            message: "Our website development agency is the best ally we discovered who actually cared about our success. Every project exhibits innovation, technical mastery, and attention to detail. The unique features and perfect operation of our website have increased user interaction and conversion rates. We appreciate their spirit of cooperation and persistent commitment to our objectives. Contact Web Design Hub and get yourself on a journey of success.",
+            name: "Michael George",
+            jd: false,
+            profile: "/website-development/reviews/3.png"
+        }
+    ]
+    return (
+        <>
+            <main>
+                {/* <PopupAlert /> */}
+                <Header />
+                <Hero content={hero} />
+                <Services />
+                <SmallBanner content={smallbanner} />
+                <Portfolios content={portfolios} />
+                <Packages content={packages} />
+                <Reviews content={reviews} />
+                <Weare />
+                <Contact />
+                <Letstalk content={letstalk} />
+                <Footer />
+                <Script id="general-schema" type="application/ld+json">
+                    {`
+                        {
+                            "@context": "http://schema.org",
+                            "@type": "Product",
+                            "name": "Website Development Company",
+                            "description": "WebDesignHub provides top-tier website development services, leveraging the latest technologies to build scalable, responsive, and user-friendly websites tailored to meet the unique needs of businesses.",
+                            "brand": {
+                                "@type": "brand",
+                                "name": "WebDesignHub"
+                            },
+                            "aggregateRating": {
+                                "@type": "AggregateRating",
+                                "ratingValue": "5",
+                                "ratingCount": "141"
+                            }
+                        }
+                    `}
+                </Script>
+            </main>
+        </>
+    );
+}
+
+export default Page;
---
+++ src/components/hero/webdevelopmentform/form.jsx
@@ -0,0 +1,349 @@
+"use client";
+// Import Components
+import {
+  Input,
+  Option,
+  Select,
+  Textarea,
+  ThemeProvider,
+} from "@material-tailwind/react";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import Axios from "axios";
+
+const WebDevelopmentForm = () => {
+  const [selectedService, setSelectedService] = useState("Not Selected");
+  const [data, setData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+    services: selectedService,
+    pageURL: usePathname(),
+  });
+  const [formStatus, setFormStatus] = useState("Submit Form");
+  const [errors, setErrors] = useState({});
+  const formValidateHandle = () => {
+    let errors = {};
+    // Name validation
+    if (!data.name.trim()) {
+      errors.name = "Name is required";
+    }
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!data.email.match(emailRegex)) {
+      errors.email = "Valid email is required";
+    }
+    // Details validation
+    if (!data.details || !data.details.trim()) {
+      errors.details = "Details is required";
+    }
+
+    // Phone validation
+    const phoneRegex = /[0-9]/i;
+    if (!data.phone.match(phoneRegex)) {
+      errors.phone = "Valid phone is required";
+    }
+    return errors;
+  };
+  const handleDataChange = (e) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleSelectServices = (e) => {
+    setSelectedService(e);
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setFormStatus("Processing...");
+
+    const errors = formValidateHandle();
+    setErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      let headersList = {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      };
+
+      let bodyContent = { ...data, services: selectedService };
+      let reqOptions = {
+        url: "/api/email",
+        method: "POST",
+        headers: headersList,
+        data: JSON.stringify(bodyContent),
+      };
+      await Axios.request(reqOptions);
+      setFormStatus("Submit Form");
+      window.location.href = "/thank-you";
+    } else {
+      setFormStatus("Failed...");
+    }
+  };
+  const theme = {
+    input: {
+      defaultProps: {
+        color: "white",
+        size: "lg",
+        variant: "outlined",
+      },
+      styles: {
+        variants: {
+          outlined: {
+            base: {
+              input: {
+                borderWidth: "border-2 placeholder-shown:border-2",
+                borderColor:
+                  "placeholder-shown:border-white placeholder-shown:border-t-white",
+                floated: {
+                  borderWidth: "border-2 focus:border-2",
+                  borderColor:
+                    "border-t-transparent focus:border-t-transparent",
+                },
+                bg: "bg-[#06050538]",
+              },
+              label: {
+                before: {
+                  floated: {
+                    bt: "before:border-t-2 peer-focus:before:border-t-2",
+                    bl: "before:border-l-2 peer-focus:before:border-l-2",
+                  },
+                },
+                after: {
+                  floated: {
+                    bt: "after:border-t-2 peer-focus:after:border-t-2",
+                    br: "after:border-r-2 peer-focus:after:border-r-2",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    select: {
+      defaultProps: {
+        variant: "outlined",
+        size: "lg",
+        color: "white", // Black is not available thts's why i use blue to override black
+      },
+      styles: {
+        base: {
+          select: {
+            color: "text-white",
+          },
+          arrow: {
+            initial: {
+              color: "text-white",
+            },
+          },
+        },
+        variants: {
+          outlined: {
+            colors: {
+              select: {
+                blue: {
+                  close: {
+                    borderColor: "border-white",
+                    // bg: "bg-[#06050538]"
+                  },
+                  open: {
+                    borderColor: "border-white",
+                    borderTopColor: "border-t-transparent",
+                    // bg: "bg-[#06050538]"
+                  },
+                  withValue: {
+                    borderColor: "border-white",
+                    borderTopColor: "border-t-transparent",
+                    // bg: "bg-[#06050538]"
+                  },
+                },
+              },
+              label: {
+                blue: {
+                  close: {
+                    color: "text-white",
+                    before: "before:border-transparent",
+                    after: "after:border-transparent",
+                  },
+                  open: {
+                    color: "text-white",
+                    before: "before:border-white",
+                    after: "after:border-white",
+                  },
+                  withValue: {
+                    color: "text-white",
+                    before: "before:border-white",
+                    after: "after:border-white",
+                  },
+                },
+              },
+            },
+            states: {
+              close: {
+                select: {
+                  borderWidth: "border-2",
+                },
+              },
+              withValue: {
+                select: {
+                  borderWidth: "border-2",
+                },
+                label: {
+                  fontSize: "text-[11px]",
+                  disabled: "peer-disabled:text-transparent",
+                  before: {
+                    bt: "before:border-t-2",
+                    bl: "before:border-l-2",
+                  },
+                  after: {
+                    bt: "after:border-t-2",
+                    br: "after:border-r-2",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    textarea: {
+      defaultProps: {
+        variant: "outlined",
+        size: "",
+        color: "blue", // Black is not available thts's why i use blue to override black
+      },
+      styles: {
+        base: {
+          textarea: {},
+        },
+        variants: {
+          outlined: {
+            base: {
+              textarea: {
+                borderWidth: "border-2 placeholder-shown:border-2",
+                borderColor:
+                  "placeholder-shown:border-white placeholder-shown:border-t-white",
+                floated: {
+                  borderWidth: "border-2 focus:border-2",
+                  borderColor:
+                    "border-t-transparent focus:border-t-transparent",
+                },
+                bg: "bg-[#06050538]",
+              },
+              label: {
+                before: {
+                  floated: {
+                    bt: "before:border-t-2 peer-focus:before:border-t-2",
+                    bl: "before:border-l-2 peer-focus:before:border-l-2",
+                  },
+                },
+                after: {
+                  floated: {
+                    bt: "after:border-t-2 peer-focus:after:border-t-2",
+                    br: "after:border-r-2 peer-focus:after:border-r-2",
+                  },
+                },
+              },
+            },
+            colors: {
+              textarea: {
+                blue: {
+                  color: "text-white",
+                  borderColor: "border-white",
+                  borderColorFocused: "focus:border-white",
+                },
+              },
+              label: {
+                blue: {
+                  color: "!text-white peer-focus:white",
+                  after: "after:border-white peer-focus:after:border-white",
+                  before: "before:border-white peer-focus:before:border-white",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  return (
+    <ThemeProvider value={theme}>
+      <div className="">
+        <div className="container">
+          <div className="bg-[#0000006b] backdrop-blur-sm py-5 lg:py-12 md:p-7 rounded-[40px] brightness-[-4] lg:max-h-[550px] flex flex-col items-center justify-between mb-5 lg:mb-0">
+            <form autoComplete="off" className="w-full px-4 lg:px-0">
+              <div className="flex flex-col gap-y-6 py-6">
+                <div className="basis-full lg:basis-1/3">
+                  <Input
+                    label="Name"
+                    type="text"
+                    name="name"
+                    onChange={handleDataChange}
+                    className="backdrop-blur-sm"
+                  />
+                  {errors.name && (
+                    <span className="text-[12px] block p-2 font-medium text-red-600">
+                      {errors.name}
+                    </span>
+                  )}
+                </div>
+                <div className="basis-full lg:basis-1/3">
+                  <Input
+                    label="Telephone Number"
+                    type="tel"
+                    name="phone"
+                    onChange={handleDataChange}
+                    className="backdrop-blur-sm"
+                  />
+                  {errors.phone && (
+                    <span className="text-[12px] block p-2 font-medium text-red-600">
+                      {errors.phone}
+                    </span>
+                  )}
+                </div>
+                <div className="basis-full lg:basis-1/3">
+                  <Input
+                    label="Email"
+                    type="email"
+                    name="email"
+                    onChange={handleDataChange}
+                    className="backdrop-blur-sm"
+                  />
+                  {errors.email && (
+                    <span className="text-[12px] block p-2 font-medium text-red-600">
+                      {errors.email}
+                    </span>
+                  )}
+                </div>
+                <div className="basis-full lg:basis-1/3">
+                  <Input
+                    label="Details"
+                    type="text"
+                    name="details"
+                    onChange={handleDataChange}
+                    className="backdrop-blur-sm"
+                  />
+                  {errors.details && (
+                    <span className="text-[12px] block p-2 font-medium text-red-600">
+                      {errors.details}
+                    </span>
+                  )}
+                </div>
+                <div className="basis-full ">
+                  <input
+                    type="button"
+                    onClick={handleFormSubmit}
+                    className="text-sm sm:text-lg font-medium pr-8 pl-8 h-11 rounded-md bg-[#0F2847] w-full text-white cursor-pointer"
+                    value={formStatus}
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default WebDevelopmentForm;
